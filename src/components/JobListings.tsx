ef0452ac7503d93ae58eb6985fcd19b86464c199
Nikolai <niklonberg@gmail.com>
2024-06-10 15:25:18 +0200

    refactor: edit JobListings component css layout

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -5,15 +5,17 @@ const JobListings = () => {
   console.log(jobs);
   return (
     <section className="bg-blue-50 px-4 py-10">
-      <h2>Browse Jobs</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {jobs.map((job) => (
-          <div key={job.id} className="bg-white p-4 shadow-md">
-            <p>{job.type}</p>
-            <h3>{job.title}</h3>
-            <p>{job.description}</p>
-          </div>
-        ))}
+      <div className="container-xl lg:container m-auto">
+        <h2>Browse Jobs</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {jobs.map((job) => (
+            <div key={job.id} className="bg-white p-4 shadow-md rounded-3xl">
+              <p>{job.type}</p>
+              <h3>{job.title}</h3>
+              <p>{job.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
