67c016a4a74807b4ea9d6d65f64e03c496a3eced
Nikolai <niklonberg@gmail.com>
2024-06-10 14:13:51 +0200

    use HeadingThree comp.

--- src/components/HomeCards.tsx
+++ src/components/HomeCards.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Card from "./Card";
+import HeadingThree from "./HeadingThree";
 
 const HomeCards = () => {
   return (
     <section className="px-4">
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <Card>
-          <h3 className="font-bold text-xl mb-2">Find a Job</h3>
+          <HeadingThree>Find a Job</HeadingThree>
           <p>Find your next job in the React ecosystem</p>
         </Card>
         <Card>
-          <h3 className="font-bold text-xl mb-2">Post a Job</h3>
+          <HeadingThree>Post a Job</HeadingThree>
           <p>Post a job to find the best React developers</p>
         </Card>
         <Card>
-          <h3 className="font-bold text-xl mb-2">Learn React</h3>
+          <HeadingThree>Learn React</HeadingThree>
           <p>Learn React with our free resources</p>
         </Card>
       </div>
