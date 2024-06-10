5aedbcf1638e2cd6b66257b6c6be04a3b48078af
Nikolai <niklonberg@gmail.com>
2024-06-10 11:51:39 +0200

    edit navbar, add links & logo img

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
     <nav>
-      Navbar here
+      <ul className="flex items-center justify-between">
+        <div>
+          <a className="flex items-center">
+            <img className="h-10 w-auto" src={logo} alt="React Jobs logo" />
+            <span>React Jobs</span>
+          </a>
+        </div>
+        <div>
+          <a href="">Home</a>
+          <a href="">Jobs</a>
+          <a href="">Add Job</a>
+        </div>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
