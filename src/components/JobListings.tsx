1fcc2440e74ce8126bd3cba12526aea24e662843
Nikolai <niklonberg@gmail.com>
2024-06-10 15:14:41 +0200

    refactor: Update JobListings component to use grid layout for job listings

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -4,11 +4,17 @@ import jobs from "../jobs.json";
 const JobListings = () => {
   console.log(jobs);
   return (
-    <section>
+    <section className="bg-blue-50 px-4 py-10">
       <h2>Browse Jobs</h2>
-      <div>Job 1</div>
-      <div>Job 2</div>
-      <div>Job 3</div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {jobs.map((job) => (
+          <div key={job.id} className="bg-white p-4 shadow-md">
+            <p>{job.type}</p>
+            <h3>{job.title}</h3>
+            <p>{job.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
