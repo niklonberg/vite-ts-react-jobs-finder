1406666e73033180347206e7f14a8f1b0a1413e5
Nikolai <niklonberg@gmail.com>
2024-06-10 11:28:49 +0200

    add basic navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='text-5xl'>
-      App
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
