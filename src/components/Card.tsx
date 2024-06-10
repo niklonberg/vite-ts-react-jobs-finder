5e4efe202b5557b5e8c01d70ac6dcdca4d722cba
Nikolai <niklonberg@gmail.com>
2024-06-10 14:22:52 +0200

    edit CSSClass prop name

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,14 @@
 import React, { PropsWithChildren } from "react";
-import { ClassName } from "../types/className";
+import { CSSClass } from "../types/CSSClass";
 
-const Card: React.FC<PropsWithChildren<ClassName>> = ({
-  className,
+const Card: React.FC<PropsWithChildren<CSSClass>> = ({
+  customClasses,
   children,
 }) => {
   return (
-    <div className={`bg-gray-100 p-6 rounded-lg shadow-md ${className ?? ""}`}>
+    <div
+      className={`bg-gray-100 p-6 rounded-lg shadow-md ${customClasses ?? ""}`}
+    >
       {children}
     </div>
   );
--- src/components/HeadingThree.tsx
+++ src/components/HeadingThree.tsx
@@ -1,12 +1,14 @@
 import React, { PropsWithChildren } from "react";
-import { ClassName } from "../types/className";
+import { CSSClass } from "../types/CSSClass";
 
-const HeadingThree: React.FC<PropsWithChildren<ClassName>> = ({
-  className,
+const HeadingThree: React.FC<PropsWithChildren<CSSClass>> = ({
+  customClasses,
   children,
 }) => {
   return (
-    <h3 className={`font-bold text-xl mb-2 ${className ?? ""}`}>{children}</h3>
+    <h3 className={`font-bold text-xl mb-2 ${customClasses ?? ""}`}>
+      {children}
+    </h3>
   );
 };
 
