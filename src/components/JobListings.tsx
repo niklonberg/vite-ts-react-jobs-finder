a7c60880cae44ff60584126b170e424f8842e114
Nikolai <niklonberg@gmail.com>
2024-06-10 15:37:39 +0200

    refactor: Add salary and "Read More" link to JobListings component

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -13,6 +13,13 @@ const JobListings = () => {
               <p>{job.type}</p>
               <h3>{job.title}</h3>
               <p>{job.description}</p>
+              <div>{`${job.salary}  / Year`}</div>
+              <a
+                href={`/jobs/${job.id}`}
+                className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-md font-semibold inline-block mt-4"
+              >
+                Read More
+              </a>
             </div>
           ))}
         </div>
