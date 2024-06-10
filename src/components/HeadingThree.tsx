3945fdde68359195e94a6cffc6c2b3960e6915da
Nikolai <niklonberg@gmail.com>
2024-06-10 14:08:22 +0200

    add HeadingThree component

---
+++ src/components/HeadingThree.tsx
@@ -0,0 +1,11 @@
+import React, { PropsWithChildren } from "react";
+import { ClassName } from "../types/className";
+
+const HeadingThree: React.FC<PropsWithChildren<ClassName>> = ({
+  className,
+  children,
+}) => {
+  return <h3 className={`font-bold text-xl mb-2 ${className}`}>{children}</h3>;
+};
+
+export default HeadingThree;
