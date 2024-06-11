79be0051ef8968d570212f616c89196abe7bdef1
Nikolai <niklonberg@gmail.com>
2024-06-11 11:51:30 +0200

    refactor: Update Navbar component layout and styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,15 @@ import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="bg-indigo-700">
-      <ul className="flex items-center justify-between">
-        <div>
-          <a className="flex items-center">
+    <nav className="bg-indigo-700 text-white ">
+      <ul className="flex items-center justify-between max-w-7xl mx-auto py-4 px-6">
+        <div className="block flex flex-shrink-0">
+          <a className="flex items-center gap-4 mr-4">
             <img className="h-10 w-auto" src={logo} alt="React Jobs logo" />
             <span className="hidden sm:block">React Jobs</span>
           </a>
         </div>
-        <div>
+        <div className="flex gap-6">
           <a href="">Home</a>
           <a href="">Jobs</a>
           <a href="">Add Job</a>
