1b060c67641b38b5dd87855b7ca08dc913ee266e
Nikolai <niklonberg@gmail.com>
2024-06-11 10:39:00 +0200

    edit JobListings css

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -7,8 +7,10 @@ const JobListings = () => {
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
-        <h2>Browse Jobs</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <h2 className="text-center text-2xl text-indigo-500 font-bold mb-4">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {jobs.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
