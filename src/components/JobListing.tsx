606c3a6f367172dacfa77dac5be0de883179214f
Nikolai <niklonberg@gmail.com>
2024-06-11 10:57:05 +0200

    refactor: Improve JobListing component layout and styling

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -7,17 +7,23 @@ interface JobListingProps {
 
 const JobListing: React.FC<JobListingProps> = ({ job }) => {
   return (
-    <div className="bg-white p-4 shadow-md rounded-2xl">
-      <p className="text-gray-500 my-2">{job.type}</p>
-      <h3 className="font-bold text-lg mb-6">{job.title}</h3>
-      <p className="mb-4">{job.description}</p>
-      <div className="border-b-2 pb-2 text-sm text-indigo-500 font-bold">{`${job.salary} / Year`}</div>
-      <a 
-        href={`/jobs/${job.id}`}
-        className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-md font-semibold inline-block mt-4"
-      >
-        Read More
-      </a>
+    <div className="bg-white p-4 shadow-md rounded-2xl flex flex-col justify-between">
+      <div>
+        <p className="text-gray-500 my-2">{job.type}</p>
+        <h3 className="font-bold text-lg mb-6">{job.title}</h3>
+        <p className="mb-4">{job.description}</p>
+        <button>More</button>
+        <div className="border-b-2 pb-2 text-sm text-indigo-500 font-bold">{`${job.salary} / Year`}</div>
+      </div>
+      <div className="flex items-center justify-between my-4">
+        <address className="text-orange-700 not-italic">{job.location}</address>
+        <a
+          href={`/jobs/${job.id}`}
+          className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-md font-semibold"
+        >
+          Read More
+        </a>
+      </div>
     </div>
   );
 };
