7e9adb7c700053b8cebb3d282f082489e31cc457
Nikolai <niklonberg@gmail.com>
2024-06-11 11:55:02 +0200

    chore: Update ViewAllJobs component layout and styling

--- src/components/ViewAllJobs.tsx
+++ src/components/ViewAllJobs.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const ViewAllJobs = () => {
   return (
-    <section>
-      <a href="">View all</a>
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        className="block bg-slate-900 text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >
+        View all
+      </a>
     </section>
   );
 };
