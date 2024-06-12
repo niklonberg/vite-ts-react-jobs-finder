04c5b2356deb2f387e9f91443f685fe0e512ce02
Nikolai <niklonberg@gmail.com>
2024-06-12 11:47:40 +0200

    refactor: Update HomeCards component structure and styling

--- src/components/HomeCards.tsx
+++ src/components/HomeCards.tsx
@@ -7,23 +7,44 @@ const HomeCards = () => {
   return (
     <section className="px-4 py-4">
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <Card>
-          <HeadingThree>For Developers</HeadingThree>
-          <CardText>Find your next job in the React ecosystem.</CardText>
-          <Link to="/jobs">Browse Jobs</Link>
+        <Card customClasses="flex flex-col justify-between items-start">
+          <div>
+            <HeadingThree>For Developers</HeadingThree>
+            <CardText>Find your next job in the React ecosystem.</CardText>
+          </div>
+          <Link
+            className="bg-indigo-500 text-white px-4 py-2 rounded-md"
+            to="/jobs"
+          >
+            Browse Jobs
+          </Link>
         </Card>
-        <Card>
-          <HeadingThree>For Employers</HeadingThree>
-          <CardText>Post a job to find the best React developers.</CardText>
-          <Link to="/add-job">Post Job</Link>
+        <Card customClasses="flex flex-col justify-between items-start">
+          <div>
+            <HeadingThree>For Employers</HeadingThree>
+            <CardText>Post a job to find the best React developers.</CardText>
+          </div>
+          <Link
+            to="/add-job"
+            className="bg-indigo-500 text-white px-4 py-2 rounded-md"
+          >
+            Post Job
+          </Link>
         </Card>
-        <Card>
-          <HeadingThree>Get Familiar With React</HeadingThree>
-          <CardText>
-            Learn React with free resources hand-picked by our senior
-            developers.
-          </CardText>
-          <Link to="/learning-resources">Learn</Link>
+        <Card customClasses="flex flex-col justify-between items-start">
+          <div>
+            <HeadingThree>Get Familiar With React</HeadingThree>
+            <CardText>
+              Learn React with free resources hand-picked by our senior
+              developers.
+            </CardText>
+          </div>
+          <Link
+            to="/learning-resources"
+            className="bg-indigo-500 text-white px-4 py-2 rounded-md"
+          >
+            Learn
+          </Link>
         </Card>
       </div>
     </section>
