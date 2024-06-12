55bcbd96b85a683ba85a8e7123418cb5aeb9cf56
Nikolai <niklonberg@gmail.com>
2024-06-12 11:08:04 +0200

    refactor: Update Navbar component with border styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="bg-indigo-700 text-white ">
+    <nav className="bg-indigo-700 text-white border-b-2 border-indigo-500">
       <ul className="flex items-center justify-between max-w-7xl mx-auto py-4 px-6">
         <div className="block flex flex-shrink-0">
           <a className="flex items-center gap-4 mr-4">
