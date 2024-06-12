0798d2429138addad6fecda5214587598700ea38
Nikolai <niklonberg@gmail.com>
2024-06-12 11:12:34 +0200

    refactor: Update Navbar component with homepage link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="bg-indigo-700 text-white border-b-2 border-indigo-500">
       <ul className="flex items-center justify-between max-w-7xl mx-auto py-4 px-6">
         <div className="block flex flex-shrink-0">
-          <a className="flex items-center gap-4 mr-4">
+          <a href="/" className="flex items-center gap-4 mr-4">
             <img className="h-10 w-auto" src={logo} alt="React Jobs logo" />
             <span className="hidden sm:block">React Jobs</span>
           </a>
