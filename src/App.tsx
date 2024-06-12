2a974bd43c6e65a8a4d3ebf98022858d038e5a85
Nikolai <niklonberg@gmail.com>
2024-06-12 16:26:25 +0200

    refactor JobPage to use data loader

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,15 @@ import MainLayout from "./layouts/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} />
+      {/* <Route path="/jobs/:id" element={<JobPage />} /> vanilla JobPage route*/}
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,32 +1,16 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  useParams,
+  useLoaderData,
+  LoaderFunctionArgs,
+  LoaderFunction,
+} from "react-router-dom";
 import { Job } from "../types/Job";
-import Spinner from "../components/Spinner";
 
 const JobPage = () => {
-  const { id } = useParams<{ id: string }>(); // Get the job ID from the URL
-  const [job, setJob] = React.useState<Job | null>(null);
-  const [isLoading, setIsLoading] = React.useState(true);
+  const { id } = useParams(); // Get the job ID from the URL
+  const job = useLoaderData() as Job;
 
-  React.useEffect(() => {
-    async function fetchJob() {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.error("Error fetching job:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchJob();
-  }, [id]);
-
-  if (isLoading) return <Spinner isLoading />;
-
-  if (!job) return <div>Job not found</div>;
+  if (!job) return <p>Job not found</p>;
 
   return (
     <section>
@@ -37,4 +21,45 @@ const JobPage = () => {
   );
 };
 
-export default JobPage;
+const jobLoader: LoaderFunction = async ({ params }: LoaderFunctionArgs) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+// 'Vanilla' way to fetch data from an API
+// const JobPage = () => {
+//   const { id } = useParams<{ id: string }>(); // Get the job ID from the URL, unsure if <{ id: string }> is necessary
+//   const [job, setJob] = React.useState<Job | null>(null);
+//   const [isLoading, setIsLoading] = React.useState(true);
+
+//   React.useEffect(() => {
+//     async function fetchJob() {
+//       try {
+//         const res = await fetch(`/api/jobs/${id}`);
+//         const data = await res.json();
+//         setJob(data);
+//       } catch (error) {
+//         console.error("Error fetching job:", error);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     }
+
+//     fetchJob();
+//   }, [id]);
+
+//   if (isLoading) return <Spinner isLoading />;
+
+//   if (!job) return <div>Job not found</div>;
+
+//   return (
+//     <section>
+//       <h1>{job.title}</h1>
+//       <p>{job.description}</p>
+//       {/* Render additional job details here */}
+//     </section>
+//   );
+// };
+
+export { JobPage as default, jobLoader };
