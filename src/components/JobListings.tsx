c31ec69e646013051bfe99f9036790c44dc53232
Nikolai <niklonberg@gmail.com>
2024-06-12 13:22:26 +0200

    change JobListings heading text based on state

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -8,7 +8,7 @@ const JobListings = ({ showAll = false }) => {
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-center text-2xl text-indigo-500 font-bold mb-4">
-          Browse Jobs
+          {showAll ? "All Jobs" : "Recent Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {jobsData.map((job) => (
