8f4be53657d0f09cb34fca345083ecf2593c5443
Nikolai <niklonberg@gmail.com>
2024-06-12 11:37:31 +0200

    refactor: Update HomeCards component with react-router links, adjust card text and fix spelling issues

--- src/components/HomeCards.tsx
+++ src/components/HomeCards.tsx
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import HeadingThree from "./HeadingThree";
 import CardText from "./CardText";
+import { Link } from "react-router-dom";
 
 const HomeCards = () => {
   return (
@@ -8,15 +9,21 @@ const HomeCards = () => {
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <Card>
           <HeadingThree>For Developers</HeadingThree>
-          <CardText>Find your next job in the React ecosystem</CardText>
+          <CardText>Find your next job in the React ecosystem.</CardText>
+          <Link to="/jobs">Browse Jobs</Link>
         </Card>
         <Card>
           <HeadingThree>For Employers</HeadingThree>
-          <CardText>Post a job to find the best React developers</CardText>
+          <CardText>Post a job to find the best React developers.</CardText>
+          <Link to="/add-job">Post Job</Link>
         </Card>
         <Card>
           <HeadingThree>Get Familiar With React</HeadingThree>
-          <CardText>Learn React with our free resources</CardText>
+          <CardText>
+            Learn React with free resources hand-picked by our senior
+            developers.
+          </CardText>
+          <Link to="/learning-resources">Learn</Link>
         </Card>
       </div>
     </section>
