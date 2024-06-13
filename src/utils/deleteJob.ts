e3a058ce60ae4b39463ae2132101d7e6f575573c
Nikolai <niklonberg@gmail.com>
2024-06-13 13:31:28 +0200

    create deleteJob util fn

---
+++ src/utils/deleteJob.ts
@@ -0,0 +1,9 @@
+async function deleteJob(id: string) {
+  // const res = await fetch(`/api/jobs/${id}`, {
+  //   method: "DELETE",
+  // });
+  console.log("deleteJob called with id: ", id);
+  return;
+}
+
+export default deleteJob;
