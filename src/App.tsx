a43ffccf00bc9cbf23cdf0f0c2369ab27e6207c7
Nikolai <niklonberg@gmail.com>
2024-06-13 12:16:07 +0200

    update: move router code inside App component

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,19 @@ import JobsPage from "./pages/JobsPage";
 import JobPage from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      {/* <Route path="/jobs/:id" element={<JobPage />} /> vanilla JobPage route*/}
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
-
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        {/* <Route path="/jobs/:id" element={<JobPage />} /> vanilla JobPage route*/}
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="/add-job" element={<AddJobPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
